3a8cf98452ad27004b71547329a97c66dad2c83b
weiding <weiding@weiding15s-mbp.attlocal.net>
2019-01-18 15:10:41 -0500

    api updateVocabHebrewBuf.js works ok.

--- utility/ndjs/weindjs_restapi/a.node.js
+++ utility/ndjs/weindjs_restapi/a.node.js
@@ -36,6 +36,55 @@ app.get("/save2afile", (req,res)=>{
   //res.send("<a/>")
   //console.log(txt);
 });
+
+
+
+
+
+
+
+function getFileNamesFromDir(startPath, filter){
+  function recursiveDir(startPath, filter, xx){
+      var files=fs.readdirSync(startPath);
+      for(var i=0;i<files.length;i++){
+          var filename=path.join(startPath,files[i]);
+          //console.log(filename);
+          var stat = fs.lstatSync(filename);
+          if (stat.isDirectory()){
+              recursiveDir(filename,filter); //recurse
+          }
+          else if (filename.indexOf(filter)>=0) {
+              //console.log('-- found: ',filename);
+              outFilesArr.push(filename);
+          };
+      };  
+  }
+  var outFilesArr=[];
+  recursiveDir(startPath,filter);
+  return outFilesArr;
+};
+function get_VocabHebrewBufObj(){
+  var currentPath = process.cwd();
+  console.log(currentPath);
+
+  var filesArr=getFileNamesFromDir("../../../../../../btool/tool/HebrewQ/audjs/",".js");
+  console.log(filesArr);
+  
+  //return;
+  var targf="../../../../../../btool/tool/HebrewQ/audjs/VocabHebrewBuf.js"
+  var content=fs.readFileSync(targf,"utf8");
+  var idx=2+content.indexOf("=\n");
+  var shead=content.substr(0,idx);
+  console.log("shead==",shead);
+  content=content.substring(idx);
+  
+  var obj=JSON.parse(content);
+  Object.keys(obj).forEach(function(k){
+    var arr=obj[k];
+    console.log(k,arr);
+  }); 
+  return {header:shead, obj:obj, fname:targf}; 
+}
 app.get("/updateVocabHebrewBuf", (req,res)=>{
   console.log("root",req.url);
 
@@ -45,6 +94,15 @@ app.get("/updateVocabHebrewBuf", (req,res)=>{
 
   q.dat=decodeURIComponent(q.dat);//must see client encodeURIComponent.
   fs.writeFileSync(q.fname, q.dat, 'utf8');
+
+  var upobj=JSON.parse(q.dat);
+  var rsObj=get_VocabHebrewBufObj();
+  Object.keys(upobj).forEach(function(k){
+    rsObj.obj[k]=upobj[k];
+  });
+  var s=rsObj.header;
+  s+=JSON.stringify(rsObj.obj,null,4);
+  fs.writeFileSync(rsObj.fname,s);
 
   
 
@@ -61,6 +119,7 @@ app.get("/updateVocabHebrewBuf", (req,res)=>{
 var port=7778;
 app.listen(7778, ()=>{
   console.log("app listern port:7778...");
+  get_VocabHebrewBufObj();
 });
 console.log("port:",port);
 ///////////////////////////////
