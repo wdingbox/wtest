f18267cf501437ba5fa888d74827e8866b057b4a
weiding <weiding@weiding15s-mbp.attlocal.net>
2019-06-01 09:08:02 -0400

    test ok

--- utility/ndjs/tool/bible_cross_reference/update_crf.nd.js
+++ utility/ndjs/tool/bible_cross_reference/update_crf.nd.js
@@ -24,21 +24,36 @@ var CrossReferenceUpdate = {
                     var txt = versObj[vers];
                     if (txt.length > 0) {
                         //console.log(bkname, chp, ":", vers, txt);
-                        CrossReferenceUpdate.searchBCV(txt);
+                        var ret = CrossReferenceUpdate.search_Ref(txt);
+                        if (ret.valid) {
+                            console.log(ret)
+                            var curBVC = `(${bkname}${chp}:${vers})`;
+                            var arr = _crf.obj[ret.book][ret.chap][ret.vers].split(";");
+                            if (arr.indexOf(curBVC) < 0) {
+                                arr.push(curBVC);
+                                _crf.obj[ret.book][ret.chap][ret.vers] = arr.join(";");
+                            }
+                        }
                     }
-
                 });
-
             });
-
         });
     },
-    searchBCV:function(txt){
-        var mat=txt.match(/([\(][A-Z0-9][A-Za-z]{2}[\s]{0,9}\d+[\:]\d+[\)])/);
-        if(mat){
-            console.log("-----")
+    search_Ref: function (txt) {
+        var ret = { book: "", chap: "", vers: "", valid: false };
+        var mat = txt.match(/([\(]\s{0,}([A-Z0-9][A-Za-z]{2})[\s]{0,}(\d+)[\s]{0,}[\:][\s]{0,}(\d+)([\-\,\d]{0,})[\)])/);
+        if (mat) {
+            console.log("-----");
             console.log(mat);
+            if (mat.length > 2) {
+                ret.book = mat[2];
+                ret.chap = mat[3];
+                ret.vers = mat[4];
+                ret.valid = true;
+            }
+
         }
+        return ret;
     }
 };////////////////////////
 
