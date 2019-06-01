d70c381226bd20f662c4d1121bc8874a5f75c5e5
weiding <weiding@weiding15s-mbp.attlocal.net>
2019-06-01 12:19:15 -0400

    work

--- utility/ndjs/tool/bible_cross_reference/update_crf.nd.js
+++ utility/ndjs/tool/bible_cross_reference/update_crf.nd.js
@@ -10,8 +10,8 @@ var tst = require("../../../../jsdb/jsBibleObj/_crf.json.js");
 
 
 var CrossReference = {
-    update: function (crf,note) {
-        console.log(note);
+    update: function (crf, note) {
+        //console.log(note);
         Object.keys(note.obj).forEach(function (bkname, i) {
             var obk = note.obj[bkname];
             Object.keys(obk).forEach(function (chp, k) {
@@ -21,8 +21,8 @@ var CrossReference = {
                     if (txt.length > 0) {
                         //console.log(bkname, chp, ":", vers, txt);
                         var ret1 = { book: bkname, chap: chp, vers: vers, valid: true };
-                        var ret2 = CrossReference.search_Ref(txt);
-                        CrossReference.cross_update(crf, ret1, ret2);
+                        var arr2 = CrossReference.search_Ref(txt);
+                        CrossReference.cross_update(crf, ret1, arr2);
                     }
                 });
             });
@@ -30,24 +30,28 @@ var CrossReference = {
     },
 
     search_Ref: function (txt) {
-        var ret = { book: "", chap: "", vers: "", valid: false };
-        var mat = txt.match(/([\(]\s{0,}([A-Z0-9][A-Za-z]{2})[\s]{0,}(\d+)[\s]{0,}[\:][\s]{0,}(\d+)([\-\,\d]{0,})[\)])/);
+        var retarr = [];
+        var mat = txt.match(/[\(]\s{0,}([A-Z0-9][A-Za-z]{2})[\s]{0,}(\d+)[\s]{0,}[\:][\s]{0,}(\d+)([\-\,\d]{0,})[\)]/g);
         if (mat) {
             console.log("-----");
             console.log(mat);
-            if (mat.length > 2) {
-                ret.book = mat[2];
-                ret.chap = mat[3];
-                ret.vers = mat[4];
-                ret.valid = true;
+            for (var i = 0; i < mat.length; i++) {
+                var bcv = mat[i];
+                var mat3 = bcv.match(/[\(]\s{0,}([A-Z0-9][A-Za-z]{2})[\s]{0,}(\d+)[\s]{0,}[\:][\s]{0,}(\d+)([\-\,\d]{0,})[\)]/);
+                if (mat3) {
+                    console.log("=======");
+                    console.log(mat3);
+                    var ret = { book: mat3[1], chap: mat3[2], vers: mat3[3], valid: true };
+                    retarr.push(ret);
+                }
             }
         }
-        return ret;
+        return retarr;
     },
     getBCV: function (ret) {
         return `(${ret.book}${ret.chap}:${ret.vers})`;
     },
-    cross_update: function (_crf, ret1, ret2) {
+    cross_update: function (_crf, ret1, arr) {
         function push2crf(_crf, ret, ret2) {
             if (false === ret.valid || false === ret2.valid) return;
             var matBVC = CrossReference.getBCV(ret2);
@@ -57,14 +61,15 @@ var CrossReference = {
                 _crf.obj[ret.book][ret.chap][ret.vers] = arr1.join(";");
             }
         }
-
-        console.log(ret2);
-        push2crf(_crf, ret1, ret2);
-        push2crf(_crf, ret2, ret1);
+        for (var i = 0; i < arr.length; i++) {
+            var ret2 = arr[i];
+            push2crf(_crf, ret1, ret2);
+            push2crf(_crf, ret2, ret1);
+        }
         _crf.writeback();
     }
 };////////////////////////
 
 var _crf = Uti.LoadObj("../../../../jsdb/jsBibleObj/_crf.json.js");
 var _bnotes = Uti.LoadObj("../../../../jsdb/jsBibleObj/_bnotes.json.js");
-CrossReference.update(_crf,_bnotes);
+CrossReference.update(_crf, _bnotes);
