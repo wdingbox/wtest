931c7675364112ae8d6639da56ae9345e3c56a76
weiding <weiding@weiding15s-mbp.attlocal.net>
2019-10-17 20:50:03 -0400

    work

--- utility/htmf/sonship/kjv_sonship/analysis/sonship.jq.js
+++ utility/htmf/sonship/kjv_sonship/analysis/sonship.jq.js
@@ -1,7 +1,7 @@
 
 
 
-function gen_table_nt(Sons) {
+function gen_table_nt___xxxx(Sons) {
     var arr = Object.keys(Sons);
     var st = "<table border='1'>";
     st += "<thead><tr><th>#</th><th>vrs</th><th>txt</th><th>usg</th><th></th></tr></thead><tbody>"
@@ -30,7 +30,32 @@ function gen_table_nt(Sons) {
 
     table_sort();
 }
+function gen_table_nt(Sons) {
+    var frqObj = [{}, {}, {}];
+    var arr = Object.keys(Sons);
+    var st = "<table border='1'>";
+    st += "<thead><tr><th>#</th><th>vrs</th><th>txt</th><th>usg</th><th></th><th></th><th></th></tr></thead><tbody>"
+    for (var i = 0; i < arr.length; i++) {
+        var key = arr[i];
+        var vrs = Sons[key];
+        var num = [0];
+        var pat1 = get_pattern_Upperxx(vrs, num, frqObj[0], "G5207");
+        var pat2 = get_pattern_lowerxx(vrs, num, frqObj[1], "G5207");
+        var pat3 = get_pattern3(vrs, num, frqObj[2], "G5207");
+        st += `<tr><td>${i}</td><td>${key}</td><td>${vrs}</td><td>${num}</td><td>${pat1}</td><td>${pat2}</td><td>${pat3}</td></tr>`;
+    }
+    st += "</tbody></table>";
+    $("#holder").html(st);
 
+
+    for (var i = 0; i < frqObj.length; i++) {
+        var sortedObj = sortObjVal(frqObj[i]);
+        frqObj[i] = sortedObj;
+    }
+    $("#out").val(JSON.stringify(frqObj, null, 4));
+
+    table_sort();
+}
 
 
 
@@ -80,9 +105,9 @@ function gen_table_ot(Sons) {
         var key = arr[i];
         var vrs = Sons[key];
         var num = [0];
-        var pat1 = get_pattern_Upperxx(vrs, num, frqObj[0]);
-        var pat2 = get_pattern_lowerxx(vrs, num, frqObj[1]);
-        var pat3 = get_pattern3(vrs, num, frqObj[2]);
+        var pat1 = get_pattern_Upperxx(vrs, num, frqObj[0], "H1121");
+        var pat2 = get_pattern_lowerxx(vrs, num, frqObj[1], "H1121");
+        var pat3 = get_pattern3(vrs, num, frqObj[2], "H1121");
         st += `<tr><td>${i}</td><td>${key}</td><td>${vrs}</td><td>${num}</td><td>${pat1}</td><td>${pat2}</td><td>${pat3}</td></tr>`;
     }
     st += "</tbody></table>";
@@ -97,9 +122,10 @@ function gen_table_ot(Sons) {
 
     table_sort();
 }
-function get_pattern3(vrs, num, frqObj) {
+function get_pattern3(vrs, num, frqObj, scod) {
+    //H1121,  G5207. 
     var patternArr = [
-        "H1121 and daughters",
+        scod+" and daughters",
         "[Yy]oung H",
         "[Hh]is son",
         "[Hh]er son",
@@ -142,12 +168,25 @@ function get_pattern3(vrs, num, frqObj) {
     }
     return "";
 }
-function get_pattern_Upperxx(vrs, num, frqObj) {
+function get_pattern_Upperxx(vrs, num, frqObj, scod) {
     vrs=vrs.replace(/[\.\,\:\'\"\?\;\'\<\>\/\-\=\_\+\(\)\*\&\^\%\$\#\@\!\~]/g, " ")+" xxx";
     var mat = vrs.match(/H1121\s+of\s+[A-Z]\w+[\,\.\'\"\:\s]+[a-zA-Z\,\.\-\:\.]+/g);
+    if(scod==="H1121"){
+        mat = vrs.match(/H1121\s+of\s+[A-Z]\w+[\,\.\'\"\:\s]+[a-zA-Z\,\.\-\:\.]+/g);
+    }
+    if(scod==="G5207"){
+        mat = vrs.match(/G5207\s+of\s+[A-Z]\w+[\,\.\'\"\:\s]+[a-zA-Z\,\.\-\:\.]+/g);
+    }
     if (mat) {
         var pattern = mat[0];
         var mat1 = pattern.match(/H1121\s+of\s+\w+/g);
+        if(scod==="H1121"){
+            mat1 = pattern.match(/H1121\s+of\s+\w+/g);
+        }
+        if(scod==="G5207"){
+            mat1 = pattern.match(/G5207\s+of\s+\w+/g);
+        }
+
         var sky = mat1[0];
         if (undefined === frqObj[sky]) {
             frqObj[sky] = 0;
@@ -158,9 +197,16 @@ function get_pattern_Upperxx(vrs, num, frqObj) {
     }
     return "";
 }
-function get_pattern_lowerxx(vrs, num, frqObj) {
+function get_pattern_lowerxx(vrs, num, frqObj, scod) {
     vrs=vrs.replace(/[\.\,\:\'\"\?\;\'\<\>\/\-\=\_\+\(\)\*\&\^\%\$\#\@\!\~]/g, " ")+" xxx";
     var mat = vrs.match(/H1121\s+of\s+[a-z]\w+[\,\.\'\"\:\s]{0,9}[a-zA-Z\,\.\-\:\.]+/g);
+    if(scod==="H1121"){
+        mat = vrs.match(/H1121\s+of\s+[a-z]\w+[\,\.\'\"\:\s]{0,9}[a-zA-Z\,\.\-\:\.]+/g);
+    }
+    if(scod==="G5207"){
+        mat = vrs.match(/G5207\s+of\s+[a-z]\w+[\,\.\'\"\:\s]{0,9}[a-zA-Z\,\.\-\:\.]+/g);
+    }
+
     if (mat) {
         var pattern = mat[0];
         var sky = pattern;
