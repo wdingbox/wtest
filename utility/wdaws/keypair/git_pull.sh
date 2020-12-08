05a072f0782b89d2b1344c5fbc62bc9f8b639643
weiding <wdingbox@gmail.com>
2020-12-07 20:07:25 -0500

    mg

---
+++ utility/wdaws/keypair/git_pull.sh
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+cd /var/www/html/wdaws/ham12/utility/ndjs/weindjs_restapi/
+pm2 start a.node.js 
+
+cd /var/www/html/wdaws/bible_concordance
+sudo git pull
+
+cd /var/www/html/wdaws/bible_obj_lib
+sudo git pull
+
+cd /var/www/html/wdaws/bible_obj_usr
+sudo git pull
+
+cd /var/www/html/wdaws/hebrew_ciu
+sudo git pull
+
