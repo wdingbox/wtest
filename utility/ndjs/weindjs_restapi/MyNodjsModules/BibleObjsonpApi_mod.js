b815523d1b4bf833baf95a79e09214ac37d698f3
weiding <wdingbox@gmail.com>
2020-12-28 19:48:08 -0500

    bad post, OPTIONS

--- utility/ndjs/weindjs_restapi/MyNodjsModules/BibleObjsonpApi_mod.js
+++ utility/ndjs/weindjs_restapi/MyNodjsModules/BibleObjsonpApi_mod.js
@@ -104,10 +104,10 @@ RunAjax : function(cbf){
     var surl = "http://${res.req.headers.host}/" + this.api
     $.ajax({
         type: "POST",
-        dataType: 'text',
+        dataType: 'json',
         contentType: "application/json; charset=utf-8",
         url: surl,
-        data: JSON.stringify(this.inp),
+        data: this.inp,
         username: 'user',
         password: 'pass',
         crossDomain : true,
