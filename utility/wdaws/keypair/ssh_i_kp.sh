115c743adf6dc57c1287f4958f78cb88075868bf
weiding <wdingbox@gmail.com>
2021-01-06 06:36:37 -0500

    auto update index.htm in root

--- utility/wdaws/keypair/ssh_i_kp.sh
+++ utility/wdaws/keypair/ssh_i_kp.sh
@@ -5,11 +5,18 @@
 
 
 
-SvrIP="152.87.154.78"
-WDKEY="wdkeypair"
+SvrIP="52.87.154.78"
+
+
+TARGF="ssed_test.txt" 
+sed -E 's|'[0-9]+.[0-9]+.[0-9]+.[0-9]+'|'"$SvrIP"'|g'  ${TARGF} > ../../../index.htm
+
+
+
 
 
 #ssh -i /path/my-key-pair.pem my-instance-user-name@my-instance-public-dns-name
+WDKEY="wdkeypair"
 ssh -i ${WDKEY}.tem ubuntu@${SvrIP}
 
 
