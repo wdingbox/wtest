1275d53a5e8f9ff056f01764e395c8b3213b0546
weiding <wdingbox@gmail.com>
2021-01-06 05:33:38 -0500

    update sh

--- utility/wdaws/keypair/ssh_i_kp.sh
+++ utility/wdaws/keypair/ssh_i_kp.sh
@@ -1,8 +1,16 @@
+#!/bin/bash
 
+
+
+
+
+
+SvrIP="152.87.154.78"
+WDKEY="wdkeypair"
 
 
 #ssh -i /path/my-key-pair.pem my-instance-user-name@my-instance-public-dns-name
-ssh -i wdkeypair.tem ubuntu@54.227.156.235
+ssh -i ${WDKEY}.tem ubuntu@${SvrIP}
 
 
 
