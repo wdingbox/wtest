c91daa08c22b4ee220ffdc7b401cbea06cd2f14a
weiding <wdingbox@gmail.com>
2021-01-06 06:40:04 -0500

    update ssh

--- utility/wdaws/keypair/ssh_i_kp.sh
+++ utility/wdaws/keypair/ssh_i_kp.sh
@@ -8,13 +8,14 @@
 SvrIP="52.87.154.78"
 
 
-TARGF="ssed_test.txt" 
-sed -E 's|'[0-9]+.[0-9]+.[0-9]+.[0-9]+'|'"$SvrIP"'|g'  ${TARGF} > ../../../index.htm
+SRC="ssed_test.txt" 
+TARGF="../../../index.htm" 
+sed -E 's|'[0-9]+.[0-9]+.[0-9]+.[0-9]+'|'"$SvrIP"'|g'  ${SRC} > ${TARGF}
 
+echo "---------------------"
+cat  ${TARGF}
 
-
-
-
+echo "---------------------"
 #ssh -i /path/my-key-pair.pem my-instance-user-name@my-instance-public-dns-name
 WDKEY="wdkeypair"
 ssh -i ${WDKEY}.tem ubuntu@${SvrIP}
