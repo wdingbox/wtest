4edb0cbe16472687087e44982e134417c6682f61
wdingsoft <wdingbox@gmail.com>
2021-01-19 09:43:12 -0500

    fs.copy if not exist

--- utility/ndjs/weindjs_restapi/MyNodjsModules/BibleObjGituser_mod.js
+++ utility/ndjs/weindjs_restapi/MyNodjsModules/BibleObjGituser_mod.js
@@ -721,6 +721,7 @@ BibleObjGituser.prototype.get_pfxname = function (DocCode) {
                 var fnam = this.get_DocCode_Fname(DocCode)
                 if (inp.usr.proj) {
                     dest_pfname = this.get_usr_myoj_dir(`/${fnam}`)
+                    ////---:
                     if (!fs.existsSync(dest_pfname)) {
                         var src = `${this.m_rootDir}bible_obj_lib/jsdb/UsrDataTemplate/myoj/${fnam}`
                         if(fs.existsSync(src)){
@@ -733,7 +734,7 @@ BibleObjGituser.prototype.get_pfxname = function (DocCode) {
                     if (!fs.existsSync(dest_pfname)) {
                         console.log("\n\n* * * [Fatal Err] missing file cannot be fixed:", dest_pfname)
                     }
-                    console.log("\n\n* * * dest_pfname:", dest_pfname)
+                    console.log("\n\n* * * my dest_pfname:", dest_pfname)
                 }
             }
             break
@@ -742,6 +743,20 @@ BibleObjGituser.prototype.get_pfxname = function (DocCode) {
                 var pfnam = DocCode.substr(1)
                 if (inp.usr.proj) {
                     dest_pfname = this.get_usr_acct_dir(`${pfnam}_json.js`)
+                    ////---: 
+                    if (!fs.existsSync(dest_pfname)) {
+                        var src = `${this.m_rootDir}bible_obj_lib/jsdb/UsrDataTemplate/dat/${fnam}`
+                        if(fs.existsSync(src)){
+                            const { COPYFILE_EXCL } = fs.constants;
+                            fs.copyFileSync(src, dest_pfname, COPYFILE_EXCL) //failed if des exists.
+                        }else{
+                            console.log("* * * [Fatal Err] src not exist:", src)
+                        }
+                    }
+                    if (!fs.existsSync(dest_pfname)) {
+                        console.log("\n\n* * * [Fatal Err] missing file cannot be fixed:", dest_pfname)
+                    }
+                    console.log("\n\n* * * my dest_pfname:", dest_pfname)
                 }
 
             }
