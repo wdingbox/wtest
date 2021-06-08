c36cc510828561d8925b4332ce3acc4c3815ae85
wdingsoft <wdingbox@gmail.com>
2021-06-08 16:07:53 -0400

    uup

--- utility/ndjs/weindjs_restapi/MyNodjsModules/BibleObjGituser_mod.js
+++ utility/ndjs/weindjs_restapi/MyNodjsModules/BibleObjGituser_mod.js
@@ -450,14 +450,14 @@ var BibleUti = {
         }
         ////SpecialTestRule: repopath must be same as password.
         inp.usr.repopath = inp.usr.repopath.trim()
-        const PUB_TEST = "pub_test"
-        if (inp.usr_proj.projname.indexOf(PUB_TEST) === 0 || "Guest" === inp.usr_proj.projname) {
-            if (inp.usr_proj.projname !== inp.usr.passcode && "3edcFDSA" !== inp.usr.passcode) {
+        const PUB_TEST = "pub_test", MYPASSWORD="3edcFDSA"
+        if (inp.usr_proj.projname.indexOf(PUB_TEST) === 0 ||  0 === inp.usr_proj.projname.indexOf("Guest") ) {
+            if (inp.usr_proj.projname !== inp.usr.passcode && MYPASSWORD !== inp.usr.passcode) {
                 console.log("This is for pub_test only but discord to the rule.")
                 return null
             } else {
                 console.log("This is for pub_test only: sucessfully pass the rule.")
-                inp.usr.passcode = "3edcFDSA"
+                inp.usr.passcode = MYPASSWORD
             }
         }
         return inp
