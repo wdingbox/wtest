d339e6685a7af41c4c313023525d860fd9c773ed
wdingsoft <wdingbox@gmail.com>
2021-09-22 19:15:07 -0400

    Jsonpster

--- utility/ndjs/weindjs_restapi/MyNodjsModules/BibleObjsonpApi_mod.js
+++ utility/ndjs/weindjs_restapi/MyNodjsModules/BibleObjsonpApi_mod.js
@@ -158,7 +158,93 @@ var ApiJsonp_BibleObj = {
 
 
 
-        var s = "var Jsonpster ="+JSON.parse(JS_Jsonpster,null,4) + "\n"+ jstr_RestApi
+        var s = `
+var Jsonpster = {
+    api: "",
+    inp: { CUID:"${sCUID}", usr:null, SSID:null, par:null, aux:null},
+    SvrUrl: "${SvrUrl}",
+    pkb64:"${pkb64}",
+getUrl : function(){
+    return this.SvrUrl + this.api
+},
+onBeforeRun : function(){
+},
+onAfterRun : function(){
+},
+
+onSignin : function(){
+    this.inp.SSID = null
+    if (!this.inp.CUID) return alert("missing CUID.")
+    if ('object' != typeof this.inp.usr)return alert("missing usr.")
+    if (this.pkb64.length === 0)return alert("no pubkey. Please load page again.")
+    
+    var usrs = JSON.stringify(this.inp.usr)
+    if (usrs.length > 500) {return alert("max 4096-bit rsa: 501B. len="+usrs.length)}
+
+    //alert(this.inp.usr)
+    var encrypt = new JSEncrypt();
+    encrypt.setPublicKey(atob(this.pkb64));
+    this.inp.cipherusrs = encrypt.encrypt(usrs);
+    this.inp.usr = null
+
+    this.api = "UsrReposPost_Signin"
+
+    console.log(this.inp.cipherusrs.length)
+    console.log(this.inp)
+    console.log("Jsonpster")
+    console.log(Jsonpster)
+},
+onSigned : function(){
+    if (this.inp.SSID === null) return alert("lost inp.SSID")
+    //if (!this.inp.par) return alert("miissing inp.par="+this.inp.par)
+    if (this.inp.usr !== null) return alert("forbidden inp.usr")
+},
+
+RunAjaxPost_Signed : function(cbf){
+    this.onSigned()
+    this.RunAjax_PostTxt (cbf)
+},
+RunAjaxPost_Signin : function (cbf) {
+    this.onSignin()
+    this.RunAjax_PostTxt (cbf)
+},
+RunAjax_PostTxt : function(cbf){
+    this.onBeforeRun()
+    if (!this.api) return alert("ErrApi="+this.api)
+    var surl = this.getUrl()
+    this.inp.api = this.api
+    $.ajax({
+        type: "POST",
+        dataType: 'text',
+        contentType: "application/json; charset=utf-8",
+        url: surl,
+        data: JSON.stringify(this.inp),
+        username: 'user',
+        password: 'pass',
+        crossDomain : true,
+        xhrFields: {
+            withCredentials: false
+        }
+    })
+        .success(function( data ) {
+            //console.log("success",data);
+            //cbf(JSON.parse(data))
+        })
+        .done(function( data ) {
+            var ret = JSON.parse(data)
+            Jsonpster.onAfterRun(ret)
+            cbf (ret)
+            Jsonpster.api = Jsonpster.inp.par = Jsonpster.inp.usr = null;
+        })
+        .fail( function(xhr, textStatus, errorThrown) {
+            console.log("surl",surl)
+            alert("xhr.responseText="+xhr.responseText+",textStatus="+textStatus);
+            //alert("textStatus="+textStatus);
+        });
+},
+}
+${jstr_RestApi}
+`;;;;;;;;;;;;;;
 
         console.log(s);
         res.send(s);
